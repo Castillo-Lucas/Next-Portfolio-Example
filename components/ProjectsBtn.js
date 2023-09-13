7cdf2ed5812a5f245ee25fea4e7093cd032e4e9c
unknown <castillolucas182@gmail.com>
2023-09-12 23:24:42 -0300

    First commit

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,5 +1,31 @@
+//Next Image
+import Image from "next/image";
+
+//Next Link
+import Link from "next/link";
+
+//Icon
+import { HiArrowDownRight } from "react-icons/hi2";
+
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return (
+    <div className="mx-auto xl:mx-0">
+      <Link
+        href={"/work"}
+        className="relative w-[135px] h-[135px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
+      >
+        <Image
+          src={"/rounded-text.png"}
+          width={105}
+          height={117}
+          alt=""
+          className="animate-spin-slow max-w-[105] max-h-[117]"
+        />
+
+        <HiArrowDownRight className="absolute text-4xl -rotate-45 group-hover:translate-x-2 transition-all duration-300" />
+      </Link>
+    </div>
+  );
 };
 
 export default ProjectsBtn;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,86 @@
+//Next Image
+import Image from "next/image";
+
+//Components
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
+//Framer Motion
+import { motion } from "framer-motion";
+
+//Variants
+import { fadeIn } from "../variants";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/60 h-full">
+      {/* Text */}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col md:justify-center pt-32 md:pt-10 xl:text-left h-full container mx-auto">
+          {/* Title */}
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
+            Soy Lucas Castillo <br />
+            <span className="text-accent">Frontend Developer</span>
+          </motion.h1>
+          {/* Subtitle */}
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="m max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10"
+          >
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Perferendis cupiditate, accusamus ducimus quos maiores id incidunt
+            sunt distinctio et expedita.
+          </motion.p>
+          {/* Mobile Button */}
+          <button className="flex justify-center xl:hidden relative">
+            <ProjectsBtn />
+          </button>
+
+          {/* Desktop Button */}
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
+        </div>
+      </div>
+      {/* Image */}
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        {/* BG Image */}
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+        {/* Particles */}
+        <div>
+          <ParticlesContainer />
+        </div>
+
+        {/* Avatar */}
+        <motion.div
+          variants={fadeIn("up", 0.5)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="w-full  absolute -bottom-32 lg:bottom-0 lg:right-[-53%]"
+        >
+          <Avatar />
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
