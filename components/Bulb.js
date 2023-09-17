1bc39acd317112cd86f631a78364b5a33f752211
unknown <castillolucas182@gmail.com>
2023-09-17 09:18:05 -0300

    Starting to Work in Services Section

--- components/Bulb.js
+++ components/Bulb.js
@@ -1,5 +1,18 @@
+//Next Image
+import Image from "next/image";
+
 const Bulb = () => {
-  return <div>Bulb</div>;
+  return (
+    <div className="hidden lg:flex absolute -left-[9%] -bottom-12 mix-blend-color-dodge animate-pulse duration-75 z-10 w-[200px] xl:w-[260px]">
+      <Image
+        src={"/bulb.png"}
+        width={260}
+        height={200}
+        className="w-full h-full"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Bulb;
--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,3 +1,11 @@
+//Swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+
+//Swiper Styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
 // icons
 import {
   RxCrop,
@@ -5,40 +13,73 @@ import {
   RxDesktop,
   RxReader,
   RxRocket,
+  RxArrowLeft,
+  RxArrowRight,
 } from "react-icons/rx";
-
 
 // data
 const serviceData = [
   {
     icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Branding",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Development",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Copywriting",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "SEO",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
 ];
 
+//Required Modes
+import { FreeMode, Pagination } from "swiper";
+
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+  return (
+    <Swiper
+      breakpoints={{
+        320: { slidesPerView: 1, spaceBetween: 15 },
+        640: { slidesPerView: 3, spaceBetween: 15 },
+      }}
+      freeMode={true}
+      pagination={{ clickable: true }}
+      modules={[FreeMode, Pagination]}
+      className="h-[240] sm:h-[340px]"
+    >
+      {serviceData.map((item, index) => (
+        <SwiperSlide key={index}>
+          <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 grid grid-cols-12  gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(100,80,162,0.15)] transition-all duration-300">
+            {/* Icon */}
+            <div className="text-4xl text-accent lg:mb-4 col-span-2 md:col-span-3">
+              {item.icon}
+            </div>
+            {/* Title & desc*/}
+            <div className="col-span-10 md:col-span-9 pt-1">{item.title}</div>
+            <div className="col-span-12 pl-14 md:pl-0">{item.description}</div>
+            {/* Arrow */}
+            <div className="text-3xl mt-8 w-fit -rotate-45 col-span-12 pl-14 md:pl-0">
+              <RxArrowRight />
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
 
 export default ServiceSlider;
