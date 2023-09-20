34e478181d4856d8121f053886359b6381d3b340
unknown <castillolucas182@gmail.com>
2023-09-20 17:47:47 -0300

    Work Section Finished

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,5 +1,43 @@
+//Components
+import WorkSlider from "../../components/WorkSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+
+//Framer Motion
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />{" "}
+      <motion.div
+        variants={fadeIn("up", 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="container mx-auto"
+      >
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          {/* Text */}
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <h2 className="h2 xl:mt8">
+              My work <span className="text-accent">.</span>
+            </h2>
+            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
+              tempora! Tempora reprehenderit, esse vitae sed ex sint ullam.
+            </p>
+          </div>
+          {/* Slider */}
+          <div className="w-full lg:w-[65%]">
+            <WorkSlider />
+          </div>
+        </div>
+      </motion.div>
+      <Bulb />
+    </div>
+  );
 };
 
 export default Work;
