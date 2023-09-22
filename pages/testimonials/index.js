892ec0b87f25a6c8f76a87eabcb7ed8d7c0c8c65
unknown <castillolucas182@gmail.com>
2023-09-22 15:47:13 -0300

    Testimonial Secction Finished

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,5 +1,21 @@
+import TestimonialSlider from "../../components/TestimonialSlider";
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center">
+      <div className="container max-auto h-full flex flex-col justify-center">
+        {/* Title */}
+        <h2 className="h2 mb-8 xl:mb-0">
+          What clients <span className="text-accent">say</span>
+        </h2>
+
+        {/* Title */}
+        <div>
+          <TestimonialSlider />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
